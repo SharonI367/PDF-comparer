3eec534edf5b06788033a880ea380fb79cf86bdf
L4ver <c5040215@newcastle.ac.uk>
2026-01-19 16:16:28 +0000

    working on word counter

--- Main.py
+++ Main.py
@@ -7,15 +7,40 @@ def first(file1, file2):
 
     with open(file1,"rb") as file:
         chunk = 0
-        while True:
+        while chunk != b'':
             chunk = file.read(1024)
             f1.update(chunk)
 
     with open(file2,"rb") as file:
         chunk = 0
-        while True:
+        while chunk != b'':
             chunk = file.read(1024)
             f2.update(chunk)
+
+        return f1.hexdigest(), f2.hexdigest()
+
+def word_count(file):
+    with open(file,"r") as f:
+        count = 0
+        for char in file.strip().rstrip("."):
+            count += 1
+            if count < 0:
+                break
+        return count
+
+
+msg1, msg2 = first("Sample_file1","Sample_file2")
+
+
+if msg1!= msg2:
+    print("Non-identical")
+    print(f"Sample_file2 word count:{word_count("Sample_file2")}")
+
+else:
+    print("identical")
+
+
+
 
 
 
